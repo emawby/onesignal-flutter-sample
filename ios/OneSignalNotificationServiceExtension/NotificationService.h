dec3b58d8f9c7445b57249628a00d788e565c15d
William Shepherd <will@codeincolor.io>
2022-01-11 16:18:45 -0600

    Bootstrap app

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by William Shepherd on 1/11/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
